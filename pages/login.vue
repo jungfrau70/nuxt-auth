7a1f4a19c04d696393f04dbc532af3a7f433481a
InHwan Jung <inhwan.jung@gmail.com>
2023-02-22 23:41:48 +0900

    first commit

---
+++ pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- <v-form v-model="valid"> -->
+    <v-form>
+      <v-container>
+        <h1>Login</h1>
+        <UserAuthForm buttonText="Login" :submitForm="loginUser" />
+      </v-container>
+    </v-form>
+  </template>
+  
+  <script>
+    import UserAuthForm from '@/components/UserAuthForm'
+
+    export default {
+        components: {
+          UserAuthForm,
+        },
+        methods: {
+          loginUser(loginInfo) {
+            this.$auth.loginWith('local', {
+              data: loginInfo
+            })
+          }
+        }
+    }
+  </script>
+
+  <style lang="scss" scoped>
+    
+  </style>
---
+++ pages/register.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-form v-model="valid">
+      <v-container>
+        <h1>Register</h1>
+        <UserAuthForm buttonText="Register" :submitForm="registerUser" hasName="true" />
+      </v-container>
+    </v-form>
+  </template>
+  
+  <script>
+    import UserAuthForm from '@/components/UserAuthForm'
+
+    export default {
+        components: {
+            UserAuthForm,
+        },
+        methods: {
+            registerUser(registerationinfo) {
+              debugger
+              alert('You pressed a button')
+            }
+        }
+    }
+  </script>
+
+  <style lang="scss" scoped>
+    
+  </style>
